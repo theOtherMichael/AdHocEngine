c543d341f89d9345347e9bf940ac15e7ee778af2
Michael Martz <michael.martz@live.com>
2023-08-20 16:01:57 -0700

    Initial commit

---
+++ Editor/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <Enterprise/Add.h>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "1 + 2 is: " << Add(1, 2) << std::endl;
+    return EXIT_SUCCESS;
+}
