88c2e041743da5409965ed7877d516837960d73d
Michael Martz <michael.martz@live.com>
2024-12-06 18:54:25 -0800

    Re-add the editor vs. engine reload flags because the new paradigm requires them

--- Editor/include/Editor/EditorReloadFlags.h
+++ Editor/include/Editor/EditorReloadFlags.h
@@ -2,11 +2,23 @@
 
 #ifdef ENTERPRISE_INTERNAL
 
-// IMPORTANT: Do NOT edit these while running the editor in developer mode!
+// IMPORTANT: Do NOT edit these while running the editor in development mode!
 
-constexpr unsigned char EditorReloadFlag_None    = 0x0;
-constexpr unsigned char EditorReloadFlag_Debug   = 0x1;
-constexpr unsigned char EditorReloadFlag_Dev     = 0x2;
-constexpr unsigned char EditorReloadFlag_Release = 0x3;
+constexpr unsigned char EditorReloadFlag_None = 0x0;
+
+// 0000PPCC
+//       ^^ config bits, two-bit number
+
+constexpr unsigned char EditorReloadFlag_Debug      = 0x1;
+constexpr unsigned char EditorReloadFlag_Dev        = 0x2;
+constexpr unsigned char EditorReloadFlag_Release    = 0x3;
+constexpr unsigned char EditorReloadFlag_ConfigMask = 0x3;
+
+// 0000PPCC
+//     ^^ project bits, two-bit bit field
+
+constexpr unsigned char EditorReloadFlag_Engine      = 0x4;
+constexpr unsigned char EditorReloadFlag_Editor      = 0x8;
+constexpr unsigned char EditorReloadFlag_ProjectMask = 0x8;
 
 #endif // ENTERPRISE_INTERNAL
