e43b0ecc65fbeb688adb1c8251c8bb168b236306
Michael Martz <michael.martz@live.com>
2024-12-27 22:28:02 -0800

    Update Xcode project to include refactored files and compile

--- Engine/include/Enterprise/Core/PlatformAbstraction.h
+++ Engine/include/Enterprise/Core/PlatformAbstraction.h
@@ -7,7 +7,7 @@
 #if defined(ENTERPRISE_WINDOWS)
     #define PLATFORM_HEADER(relativePath) STRINGIFY(CONCATENATE(_platform/Windows/Windows, relativePath))
 #elif defined(ENTERPRISE_MACOS)
-    #define PLATFORM_HEADER(relativePath) STRINGIFY(_platform/Mac/Mac # relativePath)
+    #define PLATFORM_HEADER(relativePath) STRINGIFY(CONCATENATE(_platform/Mac/Mac, relativePath))
 #else
 static_assert(false);
 #endif
