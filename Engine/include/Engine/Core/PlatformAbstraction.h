d5f5791c98298a60146e047062f2a9f20b9395aa
Michael Martz <michael.martz@live.com>
2025-10-09 22:04:48 -0700

    Add ADHOC_INJECT_PLATFORM_NAMESPACE macro

--- Engine/include/Engine/Core/PlatformAbstraction.h
+++ Engine/include/Engine/Core/PlatformAbstraction.h
@@ -13,3 +13,21 @@ static_assert(false);
 #endif
 
 // clang-format on
+
+#if ADHOC_WINDOWS
+    #define ADHOC_PLATFORM_CURRENT_NAMESPACE Windows
+#elif ADHOC_MAC
+    #define ADHOC_PLATFORM_CURRENT_NAMESPACE Mac
+#else
+static_assert(false, "ADHOC_INJECT_PLATFORM_NAMESPACE() requires a definition for ADHOC_PLATFORM_CURRENT_NAMESPACE");
+#endif
+
+#define ADHOC_INJECT_PLATFORM_NAMESPACE                                                                                \
+    namespace Platform                                                                                                 \
+    {                                                                                                                  \
+    namespace Windows                                                                                                  \
+    {}                                                                                                                 \
+    namespace Mac                                                                                                      \
+    {}                                                                                                                 \
+    namespace Current = ADHOC_PLATFORM_CURRENT_NAMESPACE;                                                              \
+    }
